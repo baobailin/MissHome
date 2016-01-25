6799f89ec81163739d1b3292295c0b63d2ac9e37
baobailin <bbllmlm@gamil.com>
2016-01-25 10:50:30 +0800

    初始化项目

---
+++ MissHome/MissHome/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MissHome
+//
+//  Created by bbl on 16/1/25.
+//  Copyright © 2016年 1yaoyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
